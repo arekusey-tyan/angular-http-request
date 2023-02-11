afaf33e1b29283efeb8346911afcd3f0db60c677
Arekusey-tyan <43652041+arekusey-tyan@users.noreply.github.com>
2023-02-11 15:29:50 +0300

    Http-Client request-response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang2';
+  title = 'HTTP-Client';
+
+  constructor(private http: HttpClient) {
+  }
+
+  onSearchFilm(props: {t: string, y: number, plot: number}) {
+    console.log(props)
+    this.http.get<any>(`http://www.omdbapi.com/?apikey=19e82d4&t=${props.t}${props.y ? `&${props.y}` : ''}${props.plot ? `&${props.plot}` : ''}`).subscribe(data => {
+      let response = document.querySelector('.response')
+      let el = `
+<div class='title'>Title: ${data.Title}</div>
+<div class='year'>Year: ${data.Year}</div>
+<div class="rating">Rating: <div>${data.Ratings[0].Source}: ${data.Ratings[0].Value}</div></div>
+<div class='title'>Runtime: ${data.Runtime}</div>
+<div class='year'>Director: ${data.Director}</div>
+<div class='year'>Actors: ${data.Actors}</div>
+<div class="plot">Plot: ${data.Plot}</div>
+<div><img src="${data.Poster}" /></div>
+`
+      response?.insertAdjacentHTML('afterbegin', el)
+    })
+  }
 }
